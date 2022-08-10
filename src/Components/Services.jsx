eea5ce76b0b75e2b6c94bf8e3f8c9d29eebaa154
Raja0514 <rajamr1987@gmail.com>
2022-08-10 16:49:35 +0400

    fffffggg

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Services = () => {
   return (
     <section id="service">
-      <div className="container-fluid mt-5">
-        <div className="row mb-5 ">
+      <div className="container-fluid mt-5 py-5">
+        <div className="row mb-5  ">
           <div className="col mb-2">
             <h3 className="fs-5 text-center ">Our Services</h3>
             <h1 className="display-6 text-center">
@@ -14,8 +14,8 @@ const Services = () => {
           </div>
         </div>
           <div className="row mb-5  ">
-            <div className="col mb-2">
-              <h5 className="text-center mb-1 ">IFB -Wj</h5>
+            <div className="col mb-3">
+              <h5 className="text-center mb-3 ">IFB -Wj</h5>
               <img
                 src="/assets/flow3.jpg"
                 alt="err"
@@ -23,13 +23,13 @@ const Services = () => {
               />
             </div>
 
-            <div className="col mb-2">
-              <h5 className="text-center mb-1">NOAT-CNC Matrix</h5>
+            <div className="col mb-3">
+              <h5 className="text-center mb-3">NOAT-CNC Matrix</h5>
               <img src="/assets/jpk.jpg" alt="err" className="contact-file1" />
             </div>
 
-            <div className="col mb-2">
-              <h5 className="text-center mb-1 ">Dynamic- XD Wj</h5>
+            <div className="col mb-3">
+              <h5 className="text-center mb-3 ">Dynamic- XD Wj</h5>
               <img
                 src="/assets/flow2.jpg"
                 alt="err"
