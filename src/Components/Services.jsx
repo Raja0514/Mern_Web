40d8a2f20bb97a8187fe34ca07cc2848c953de77
Raja0514 <rajamr1987@gmail.com>
2022-08-10 16:43:45 +0400

    vvvv

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,69 +1,45 @@
 import React from "react";
 
 const Services = () => {
-
   return (
-
-    <div className="container-fluid">
-
-      <section id="service">
-
-        <div className="container-fluid py-5">
-
-          <div className="row">
-
-            <div className="col-md-12">
-
-              <h3 className="fs-5 text-center mb-1">Our Services</h3>
-              <h1 className="display-6 text-center">
-                Our <b>Marble Cutting Technology</b> servies
-              </h1>
-              <hr className="w-25 mx-auto mb-4" />
-
-            </div>
-
-            <div className="row mb-4 py-2">
-
-              <div className="col-md-4 ">
-                <h5 className="text-center p-1">IFB -Wj</h5>
-                <img
-                  src="/assets/flow3.jpg"
-                  alt="err"
-                  className="contact-file1"
-                />
-              </div>
-
-              <div className="col-md-4 ">
-                <h5 className="text-center p-1">NOAT-CNC Matrix</h5>
-                <img
-                  src="/assets/jpk.jpg"
-                  alt="err"
-                  className="contact-file1"
-                />
-              </div>
-
-              <div className="col-md-4 ">
-                <h5 className="text-center p-1">Dynamic- XD Wj</h5>
-                <img
-                  src="/assets/flow2.jpg"
-                  alt="err"
-                  className="contact-file1"
-                />
-              </div>
-
-
-
-            </div>
-            
+    <section id="service">
+      <div className="container-fluid mt-5">
+        <div className="row mb-5 ">
+          <div className="col mb-2">
+            <h3 className="fs-5 text-center ">Our Services</h3>
+            <h1 className="display-6 text-center">
+              Our <b>Marble Cutting Technology</b> servies
+            </h1>
+            <hr className="w-25 mx-auto " />
           </div>
-
         </div>
+          <div className="row mb-5  ">
+            <div className="col mb-2">
+              <h5 className="text-center mb-1 ">IFB -Wj</h5>
+              <img
+                src="/assets/flow3.jpg"
+                alt="err"
+                className="contact-file1"
+              />
+            </div>
 
-      </section>
+            <div className="col mb-2">
+              <h5 className="text-center mb-1">NOAT-CNC Matrix</h5>
+              <img src="/assets/jpk.jpg" alt="err" className="contact-file1" />
+            </div>
 
-
-
-    </div>
+            <div className="col mb-2">
+              <h5 className="text-center mb-1 ">Dynamic- XD Wj</h5>
+              <img
+                src="/assets/flow2.jpg"
+                alt="err"
+                className="contact-file1"
+              />
+            </div>
+          </div>
+        </div>
+      
+    </section>
   );
 };
 export default Services;
